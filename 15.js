e5c60932bfc502d22d369aeaff66b489aab05df8
chobyn <chobynleo@gmail.com>
2022-05-01 21:14:17 +0800

    fix 15

--- 15.js
+++ 15.js
@@ -5,6 +5,8 @@
  var threeSum = function(nums) {
     const len = nums.length;
     if(len < 3) return [];
+    // 排序的目的是减少搜索的范围，提高效率
+    // 如果穷举效率低，排序后如果nums[i] > 0 是没有必要继续运算的
     nums.sort(function(a,b){return a-b});   
     let res = [];
     for (let i = 0; i < len - 2; i++) {
