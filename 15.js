ca29fe9383250fe9989a05323851e64e9528703d
chobyn <chobynleo@gmail.com>
2022-05-01 21:12:31 +0800

    add 15

---
+++ 15.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var threeSum = function(nums) {
+    const len = nums.length;
+    if(len < 3) return [];
+    nums.sort(function(a,b){return a-b});   
+    let res = [];
+    for (let i = 0; i < len - 2; i++) {
+        if(nums[i] > 0) break;
+        // a去重
+        if(i > 0 && nums[i] === nums[i-1]) continue;
+        let l = i + 1, r = len - 1;
+        while(l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
+            if(sum < 0) {l++; continue};
+            if(sum > 0) {r--; continue};
+            res.push([nums[i], nums[l], nums[r]])
+            
+            // bc 去重
+            while(l < r && nums[l] === nums[++l] );
+            while(l < r && nums[r] === nums[--r] );
+        }
+    }
+
+    return res;
+};
