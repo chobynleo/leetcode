c160519a299bd422e77e19c4c3c812ffdf593e61
chobyn <chobynleo@gmail.com>
2022-06-06 17:45:10 +0800

    fix 104 增加递归法

--- 104.js
+++ 104.js
@@ -6,7 +6,52 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
 /**
+ * @dev 后序遍历
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(!root) return 0;
+
+    // 左
+    var leftMaxDepth = maxDepth(root.left);
+    // 右
+    var rightMaxDepth = maxDepth(root.right);
+    // 中
+    var childMaxDepth = Math.max(leftMaxDepth, rightMaxDepth);
+    
+    return 1 + childMaxDepth;
+};
+
+/**
+ * @dev 前序遍历
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
+    // 全局的最大深度结果
+    var maxDepthResult = 0;
+    // 先创建一个递归函数
+    var getDepth = function(node, depth) {
+        // 中
+        maxDepthResult = depth > maxDepthResult? depth : maxDepthResult;
+        // 左
+        if(node.left) getDepth(node.left, depth + 1);
+        // 右
+        if(node.right) getDepth(node.right, depth + 1);
+
+        return ;
+    }
+
+    if(!root) return maxDepthResult;
+    getDepth(root, 1);
+    return maxDepthResult;
+};
+
+/**
+ * @dev 层序遍历
  * @param {TreeNode} root
  * @return {number}
  */
