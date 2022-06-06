2fa16ce64642afba93e9405c929d17e3da2bcae7
chobyn <chobynleo@gmail.com>
2022-06-06 17:45:28 +0800

    fix 增加递归法

--- 111.js
+++ 111.js
@@ -10,6 +10,31 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// 递归法-深度优先遍历-后序遍历
+var minDepth = function(root) {
+
+    if(!root) return 0;
+
+    // 左
+    var leftMinDepth = minDepth(root.left);
+    // 右
+    var rightMinDepth = minDepth(root.right);
+
+    if(!root.left && root.right) {
+        return 1 + rightMinDepth
+    }
+
+    if(!root.right && root.left) {
+        return 1 + leftMinDepth
+    }
+    
+    // 中
+    var childMinDepth = Math.min(leftMinDepth, rightMinDepth);
+    
+    return 1 + childMinDepth;
+};
+
+// 迭代法-广度优先遍历-层次遍历
 var minDepth = function(root) {
     var depth = 0, queue = [];
 
