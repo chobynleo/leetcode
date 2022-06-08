9bbeb439ce622d1a43d75c35f01e36164c52617d
chobyn <chobynleo@gmail.com>
2022-06-08 19:33:13 +0800

    fix 111

--- 111.js
+++ 111.js
@@ -10,6 +10,37 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+// 递归法-深度优先遍历-前序遍历
+var minDepth = function(root) {
+    if(!root) return 0;
+
+    // 全局的最小深度结果
+    var minDepthResult = 100000000;
+
+    // 先创建一个递归函数
+    var getDepth = function(node, depth) {
+        
+        // 中 等同于先处理 node.val,这里只是对每一个节点进行标记，标记深度而已
+        if(!node.left && !node.right) {
+            if(depth < minDepthResult) minDepthResult = depth;
+        }
+
+        var nextDepth = depth + 1;
+
+        // 左
+        if(node.left) getDepth(node.left, nextDepth);
+
+        // 右
+        if(node.right) getDepth(node.right, nextDepth);
+
+    }
+
+    getDepth(root, 1);
+
+    return minDepthResult;
+};
+
 // 递归法-深度优先遍历-后序遍历
 var minDepth = function(root) {
 
