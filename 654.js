1fd03073f2701ce5e8713f5cbe5658e8a6f69d6a
chobyn <chobynleo@gmail.com>
2022-06-11 15:57:44 +0800

    fix 654 最大二叉树

--- 654.js
+++ 654.js
@@ -10,6 +10,29 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
+// 官方解法
+var constructMaximumBinaryTree = function (nums) {
+    const BuildTree = (arr, left, right) => {
+        if (left > right)
+            return null;
+        let maxValue = -1;
+        let maxIndex = -1;
+        for (let i = left; i <= right; ++i) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+                maxIndex = i;
+            }
+        }
+        let root = new TreeNode(maxValue);
+        root.left = BuildTree(arr, left, maxIndex - 1);
+        root.right = BuildTree(arr, maxIndex + 1, right);
+        return root;
+    }
+    let root = BuildTree(nums, 0, nums.length - 1);
+    return root;
+};
+
+// 我的解法
 var getMax = function(nums) {
     var max = -1, maxIndex;
     for (let i = 0; i < nums.length; i++) {
@@ -24,7 +47,7 @@ var constructMaximumBinaryTree = function(nums) {
     if(!nums.length) return null;
 
     var midIndex = getMax(nums);
-    
+
     var midNode = new TreeNode(nums[midIndex]);
 
     var leftArr = nums.slice(0, midIndex);
