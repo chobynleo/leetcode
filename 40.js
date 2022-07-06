6a6747537032b8cd1804ac3ef6d345af9ad2f88e
chobyn <chobynleo@gmail.com>
2022-07-06 16:35:39 +0800

    add 40. 组合总和 II

--- 40.js
+++ 40.js
@@ -5,18 +5,21 @@
  */
 var combinationSum2 = function(candidates, target) {
     var res = [], cur = [];
-
+    candidates.sort((a, b) => a - b); // 排序
     var backTracking = function(start) {
-        if(start >= candidates.length) return;
-
-        var sum = res.length == 0? 0 : cur.reduce((a, b) => a + b);
-        if(sum >= target) {
-            if(sum == target) res.push([...cur]);
+        var sum = cur.length == 0? 0 : cur.reduce((a, b) => a + b);
+        if(sum == target) {
+            res.push([...cur]);
             return;
         }
 
         for (let i = start; i < candidates.length; i++) {
+            // 剪枝优化
+            if(sum + candidates[i] > target) break;
+            // 去重
+            if(i > start && candidates[i] == candidates[i - 1]) continue;
             cur.push(candidates[i]);
+            pre = candidates[i];
             backTracking(i + 1);
             cur.pop();
         }
