39a95d5ec150adca840906544731fe7c29cedbd9
chobyn <chobynleo@gmail.com>
2022-07-08 10:41:47 +0800

    fix 90. 子集 II

--- 90.js
+++ 90.js
@@ -5,20 +5,12 @@
 var subsetsWithDup = function(nums) {
     var res= [], path = [];
     nums = nums.sort((a, b) => a-b);
-    var isRepeat = function(s, l, r, target) {
-        for (let i = l; i <= r; i++) {
-             if(target == s[i]) return true;
-        }
 
-        return false;
-    }
     var backTracking = function(start) {
         res.push([...path]);
 
         for (let i = start; i < nums.length; i++) {    
-            if(i > start) {
-                if(isRepeat(nums, start, i - 1, nums[i])) continue;
-            }
+            if(i > start && nums[i] == nums[i -1]) continue;
             path.push(nums[i]);
             backTracking(i + 1);
             path.pop();
