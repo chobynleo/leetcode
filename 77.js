93a56257c675efb48e13fba373988a42816a1af9
chobyn <chobynleo@gmail.com>
2022-07-08 10:58:24 +0800

    fix 77. 组合

--- 77.js
+++ 77.js
@@ -27,25 +27,22 @@ var combine = function(n, k) {
 
 // 剪枝法
 var combine = function(n, k) {
-    var res = [], path = [];
-
-    var backTracking  = (n, k, m) => {
-        if(path.length == k) {
-            res.push([...path]);
+    var res = [], cur = [];
+    var backTracking = function(start) {
+        if(cur.length == k) {
+            res.push([...cur]);
             return;
         }
-
-        // 即当前的序号i加上补位数组的和要小于等于n
-        // i + k - (len + 1) <= n
+        // 即当前的序号i加上补位数组以及已加入的当前cur数组的长度要大于等于k
         // 1 + n - i + len >= k
-        for (let i = m; i <= n - k + (path.length + 1); i++) {
-            path.push(i);
-            backTracking(n, k, i + 1);
-            path.pop();
+        for (let i = start; i <= (1 + n + cur.length - k); i++) {
+            cur.push(i);
+            backTracking(i + 1);
+            cur.pop();
         }
     }
-    
-    backTracking(n, k, 1);
+
+    backTracking(1);
 
     return res;
 };
