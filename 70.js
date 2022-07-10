104d04e4e61cb8a1fd7d6141f3e5a55ae8f157ce
chobyn <chobynleo@gmail.com>
2022-07-10 21:41:29 +0800

    add 70. 爬楼梯

---
+++ 70.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// 时间复杂度：O(n) 空间复杂度：O(n)
+var climbStairs = function(n) {
+    if(n < 3) return n;
+
+    var dp = [0, 1, 2];
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+};
+
+// 时间复杂度：O(n) 空间复杂度：O(1)
+var climbStairs = function(n) {
+    if(n < 3) return n;
+
+    var dp = [0, 1, 2];
+    for (let i = 3; i <= n; i++) {
+        var sum = dp[1] + dp[2];
+        dp[1] = dp[2];
+        dp[2] = sum;
+    }
+
+    return dp[2];
+};
