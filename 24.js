882fe6e30fef37bed815aef157f938ed727f2b15
chobyn <chobynleo@gmail.com>
2022-09-12 17:26:19 +0800

    fix 24. 两两交换链表中的节点

--- 24.js
+++ 24.js
@@ -1,13 +1,29 @@
-// 24 两两交换节点
-var swapPairs = function(head){
-    var virtualHead = new ListNode(0, head), cur = virtualHead
-    while(cur.next && cur.next.next){
-        var pre = cur.next
-        var temp = cur.next.next.next
-        cur.next = cur.next.next
-        cur.next.next = pre
-        pre.next = temp
-        cur = pre
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(!head) return head
+
+    var virtualHead = new ListNode(0, head)
+    var cur = virtualHead
+
+    while(cur.next && cur.next.next) {
+        var temp1 = cur.next
+        var temp2 = cur.next.next
+
+        temp1.next = temp2.next
+        cur.next = temp2
+        temp2.next = temp1
+        cur = temp1
     }
+
     return virtualHead.next
-}
+};
