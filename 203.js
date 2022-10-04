44e3287d05ae92e6cacb5ce652efe0973cda0470
chobyn <chobynleo@gmail.com>
2022-10-04 15:12:13 +0800

    fix: 203. 移除链表元素

--- 203.js
+++ 203.js
@@ -11,6 +11,14 @@
  * @return {ListNode}
  */
  var removeElements = function(head, val) {
+    if(!head) return null;
+
+    head.next = removeElements(head.next, val);
+
+    return head.val == val? head.next : head;
+};
+
+ var removeElements = function(head, val) {
   if(!head) return head
 
   var virtualHead = new ListNode(0, head)
